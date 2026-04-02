90c37fbf4ac2fe22a4b4b92eeea3ca997f290a89
FBW81C <fbw81c@gmail.com>
2026-04-02 15:52:28 +0200

    Add basic settings in settings form

--- OpenDownloader/SettingsForm.Designer.cs
+++ OpenDownloader/SettingsForm.Designer.cs
@@ -31,11 +31,18 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
             btn_save = new Button();
             btn_cancel = new Button();
+            gb_notifications = new GroupBox();
+            cb_showNotifications = new CheckBox();
+            tb_notificationDuration = new TrackBar();
+            lbl_notificationDuration = new Label();
+            gb_notifications.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)tb_notificationDuration).BeginInit();
             SuspendLayout();
             // 
             // btn_save
             // 
-            btn_save.Location = new Point(600, 380);
+            btn_save.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btn_save.Location = new Point(1020, 1054);
             btn_save.Name = "btn_save";
             btn_save.Size = new Size(188, 58);
             btn_save.TabIndex = 0;
@@ -44,12 +51,55 @@
             // 
             // btn_cancel
             // 
-            btn_cancel.Location = new Point(406, 380);
+            btn_cancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btn_cancel.Location = new Point(826, 1054);
             btn_cancel.Name = "btn_cancel";
             btn_cancel.Size = new Size(188, 58);
             btn_cancel.TabIndex = 1;
             btn_cancel.Text = "Cancel";
             btn_cancel.UseVisualStyleBackColor = true;
+            // 
+            // gb_notifications
+            // 
+            gb_notifications.Controls.Add(lbl_notificationDuration);
+            gb_notifications.Controls.Add(tb_notificationDuration);
+            gb_notifications.Controls.Add(cb_showNotifications);
+            gb_notifications.Location = new Point(12, 12);
+            gb_notifications.Name = "gb_notifications";
+            gb_notifications.Size = new Size(918, 395);
+            gb_notifications.TabIndex = 2;
+            gb_notifications.TabStop = false;
+            gb_notifications.Text = "Notifications";
+            // 
+            // cb_showNotifications
+            // 
+            cb_showNotifications.AutoSize = true;
+            cb_showNotifications.Location = new Point(27, 68);
+            cb_showNotifications.Name = "cb_showNotifications";
+            cb_showNotifications.Size = new Size(304, 45);
+            cb_showNotifications.TabIndex = 0;
+            cb_showNotifications.Text = "Show Notifications";
+            cb_showNotifications.UseVisualStyleBackColor = true;
+            // 
+            // tb_notificationDuration
+            // 
+            tb_notificationDuration.AccessibleName = "";
+            tb_notificationDuration.Location = new Point(27, 190);
+            tb_notificationDuration.Maximum = 20;
+            tb_notificationDuration.Minimum = 1;
+            tb_notificationDuration.Name = "tb_notificationDuration";
+            tb_notificationDuration.Size = new Size(546, 114);
+            tb_notificationDuration.TabIndex = 1;
+            tb_notificationDuration.Value = 1;
+            // 
+            // lbl_notificationDuration
+            // 
+            lbl_notificationDuration.AutoSize = true;
+            lbl_notificationDuration.Location = new Point(27, 134);
+            lbl_notificationDuration.Name = "lbl_notificationDuration";
+            lbl_notificationDuration.Size = new Size(364, 41);
+            lbl_notificationDuration.TabIndex = 2;
+            lbl_notificationDuration.Text = "Notification Duration (sec)";
             // 
             // SettingsForm
             // 
@@ -57,12 +107,16 @@
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
             CancelButton = btn_cancel;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1220, 1124);
+            Controls.Add(gb_notifications);
             Controls.Add(btn_cancel);
             Controls.Add(btn_save);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "SettingsForm";
             Text = "OpenDownloader Settings";
+            gb_notifications.ResumeLayout(false);
+            gb_notifications.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)tb_notificationDuration).EndInit();
             ResumeLayout(false);
         }
 
@@ -70,5 +124,9 @@
 
         private Button btn_save;
         private Button btn_cancel;
+        private GroupBox gb_notifications;
+        private CheckBox cb_showNotifications;
+        private TrackBar tb_notificationDuration;
+        private Label lbl_notificationDuration;
     }
 }
--- OpenDownloader/model/Settings.cs
+++ OpenDownloader/model/Settings.cs
@@ -7,9 +7,12 @@ using System.Threading.Tasks;
 namespace OpenDownloader.model;
 public class Settings
 {
+    // Downloading
     public string DefaultSaveDirectory { get; set; } = string.Empty;
+    // Notifications
     public bool ShowNotifications { get; set; } = true;
     public int NotificationDurationMs { get; set; } = 3000;
+    // Logging
     public bool AutoSaveLog { get; set; } = false;
     public string LogSaveDirectory { get; set; } = string.Empty;
 }
